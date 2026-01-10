34457dac20d5eb7965b8777a350523335a49bcf8
clintonbampoe <clintbamp@gmail.com>
2026-01-10 19:40:50 +0000

    Refactored table class to manage system resources effectively

--- Console Apps/Habit Logger/Table.cs
+++ Console Apps/Habit Logger/Table.cs
@@ -7,15 +7,17 @@ class Table
     {
         public static void New(SqliteConnection connection)
         {
-            SqliteCommand createHabitsTable = connection.CreateCommand();
-            createHabitsTable.CommandText =
-            @"CREATE TABLE IF NOT EXISTS Habit (
-            Name TEXT NOT NULL,
-            Date TEXT NOT NULL,
-            Occurance INT DEFAULT 0,
-            PRIMARY KEY (Name, Date)
-            );";
-            createHabitsTable.ExecuteNonQuery();
+            using(SqliteCommand createHabitsTable = connection.CreateCommand())
+            {
+                createHabitsTable.CommandText =
+                @"CREATE TABLE IF NOT EXISTS Habit (
+                Name TEXT NOT NULL,
+                Date TEXT NOT NULL,
+                Occurance INT DEFAULT 0,
+                PRIMARY KEY (Name, Date)
+                );";
+                createHabitsTable.ExecuteNonQuery();
+            }
         }
     }
 
@@ -23,21 +25,25 @@ class Table
     {
         public static void New(SqliteConnection connection, string name, string date, int occurance)
         {
-            SqliteCommand InsertHabitCmd = connection.CreateCommand();
-            InsertHabitCmd.CommandText = "INSERT OR IGNORE INTO Habit (Name, Date, Occurance) VALUES ($name, $date, $occurance)";
-            InsertHabitCmd.Parameters.AddWithValue("$name", name);
-            InsertHabitCmd.Parameters.AddWithValue("$date", date);
-            InsertHabitCmd.Parameters.AddWithValue("$occurance", occurance);
-            InsertHabitCmd.ExecuteNonQuery();
+            using (SqliteCommand InsertHabitCmd = connection.CreateCommand())
+            {
+                InsertHabitCmd.CommandText = "INSERT OR IGNORE INTO Habit (Name, Date, Occurance) VALUES ($name, $date, $occurance)";
+                InsertHabitCmd.Parameters.AddWithValue("$name", name);
+                InsertHabitCmd.Parameters.AddWithValue("$date", date);
+                InsertHabitCmd.Parameters.AddWithValue("$occurance", occurance);
+                InsertHabitCmd.ExecuteNonQuery();
+            }
         }
 
         public static void NameAndDateOnly(SqliteConnection connection, string name, string date)
         {
-            SqliteCommand InsertHabitCmd = connection.CreateCommand();
-            InsertHabitCmd.CommandText = "INSERT OR IGNORE INTO Habit (Name, Date) VALUES ($name, $date)";
-            InsertHabitCmd.Parameters.AddWithValue("$name", name);
-            InsertHabitCmd.Parameters.AddWithValue("$date", date);
-            InsertHabitCmd.ExecuteNonQuery();
+            using(SqliteCommand InsertHabitCmd = connection.CreateCommand())
+            {
+                InsertHabitCmd.CommandText = "INSERT OR IGNORE INTO Habit (Name, Date) VALUES ($name, $date)";
+                InsertHabitCmd.Parameters.AddWithValue("$name", name);
+                InsertHabitCmd.Parameters.AddWithValue("$date", date);
+                InsertHabitCmd.ExecuteNonQuery();
+            }
         }
     }
 
@@ -53,9 +59,9 @@ class Table
                     ORDER BY Name, Date
                     LIMIT 1 OFFSET $rowMinusOne
                 );";
-                    updateRowCmd.Parameters.AddWithValue("$updateOccurance", occurance);
-                    updateRowCmd.Parameters.AddWithValue("$rowMinusOne", row-1);
-                    updateRowCmd.ExecuteNonQuery();
+                updateRowCmd.Parameters.AddWithValue("$updateOccurance", occurance);
+                updateRowCmd.Parameters.AddWithValue("$rowMinusOne", row-1);
+                updateRowCmd.ExecuteNonQuery();
             }
         }
     }
@@ -63,22 +69,25 @@ class Table
     {
         public static void All(SqliteConnection connection)
         {
-            SqliteCommand selectCommand = connection.CreateCommand();
-            selectCommand.CommandText = "SELECT * FROM Habit ORDER BY name, date";
-
-            using (SqliteDataReader reader = selectCommand.ExecuteReader())
+            using (SqliteCommand selectCommand = connection.CreateCommand())
             {
-                int entryNo = 0;
-                while (reader.Read())
-                {
-                    entryNo++;
-                    string name = reader.GetString(0);
-                    string date = reader.GetString(1);
-                    int occurance = reader.GetInt32(2);
+                selectCommand.CommandText = "SELECT * FROM Habit ORDER BY name, date";
 
-                    Console.WriteLine("| {0, 3} | {1,-20} | {2,-20} | {3,4} |", entryNo, name, date, occurance);
+                using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                {
+                    int entryNo = 0;
+                    while (reader.Read())
+                    {
+                        entryNo++;
+                        string name = reader.GetString(0);
+                        string date = reader.GetString(1);
+                        int occurance = reader.GetInt32(2);
+
+                        Console.WriteLine("| {0, 3} | {1,-20} | {2,-20} | {3,4} |", entryNo, name, date, occurance);
+                    }
                 }
             }
+           
         }
     }
 
@@ -86,23 +95,26 @@ class Table
     {
         public static void Table(SqliteConnection connection)
         {
-            SqliteCommand deleteTableCmd = connection.CreateCommand();
-            deleteTableCmd.CommandText = "DROP TABLE Habit";
-            deleteTableCmd.ExecuteNonQuery();
+            using(SqliteCommand deleteTableCmd = connection.CreateCommand())
+            {
+                deleteTableCmd.CommandText = "DROP TABLE Habit";
+                deleteTableCmd.ExecuteNonQuery();
+            }
         }
 
         public static void Row(SqliteConnection connection, int row)
         {
-            SqliteCommand deleteRowCmd = connection.CreateCommand();
-            deleteRowCmd.CommandText =
-            @"DELETE FROM Habit WHERE rowid = (
-                SELECT rowid FROM Habit
-                ORDER BY name, date
-                LIMIT 1 OFFSET $rowMinusOne
-            );";
-            deleteRowCmd.Parameters.AddWithValue("$rowMinusOne", row - 1);
-            deleteRowCmd.ExecuteNonQuery();
-                
+            using(SqliteCommand deleteRowCmd = connection.CreateCommand())
+            {
+                deleteRowCmd.CommandText =
+                @"DELETE FROM Habit WHERE rowid = (
+                    SELECT rowid FROM Habit
+                    ORDER BY name, date
+                    LIMIT 1 OFFSET $rowMinusOne
+                );";
+                deleteRowCmd.Parameters.AddWithValue("$rowMinusOne", row - 1);
+                deleteRowCmd.ExecuteNonQuery();
+            }    
         }
     }
 }
