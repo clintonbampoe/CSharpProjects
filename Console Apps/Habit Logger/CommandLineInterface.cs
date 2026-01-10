49f31c3e43844e402f16bf28232b0e1adffe1d48
clintonbampoe <clintbamp@gmail.com>
2026-01-10 01:47:58 +0000

    Main menu and welcome screen of CLI completed

---
+++ Console Apps/Habit Logger/CommandLineInterface.cs
@@ -0,0 +1,40 @@
+﻿using HabitLogger;
+class CommandLineInterface 
+{ 
+    public static void Run()
+    {
+        Console.Title = "THE HABIT LOGGER";
+        Console.WriteLine("Press any key to Proceed...");
+        Console.ReadKey();
+
+        Console.Clear();
+        Method.Formatting.HorizontalLine(50);
+        Console.WriteLine();
+
+        Console.WriteLine("Welcome to THE HABIT LOGGER");
+        Console.WriteLine("\t n - Create New Habit");
+        Console.WriteLine("\t u - Update Existing Habit");
+
+        Console.WriteLine("Enter");
+        Console.Write(">>> ");
+        string input = Method.Input.Take();
+
+        while (!(input.ToLower() == "n") && !(input.ToLower() == "u"))
+        {
+            Method.Print.RedText("Invalid Input");
+            Method.Print.RedText("Please Try Again");
+            Console.Write(">>> ");
+            input = Method.Input.Take();
+        }
+
+        if(input.ToLower() == "n")
+        {
+            // create new habit in database
+        }
+        else
+        {
+            // perform C.R.U.D operations on existing habit
+        }
+
+    }
+}
