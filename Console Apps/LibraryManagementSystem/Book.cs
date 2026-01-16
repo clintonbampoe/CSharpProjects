2422dc7b1fc02b950219877b14b8233611f32c26
clintonbampoe <clintbamp@gmail.com>
2026-01-16 14:27:00 +0000

    Constructors: TCSA OOP Guide

---
+++ Console Apps/LibraryManagementSystem/Book.cs
@@ -0,0 +1,25 @@
+﻿class Book 
+{
+    string Name = "Unknown";
+    int Pages = 0;
+
+    internal Book(string name, int pages)
+    {
+        Name = name;
+        Pages = pages;
+        Console.WriteLine($"Name: {Name}, Pages: {Pages}");
+    }
+
+    internal Book(int pages)
+    {
+        Pages = pages;
+        Console.WriteLine($"Name: {Name}, Pages: {Pages}");
+    }
+
+    internal Book()
+    {
+        Name = "Unknown";
+        Pages = 0;
+        Console.WriteLine($"Name: {Name}, Pages: {Pages}");
+    }
+}
