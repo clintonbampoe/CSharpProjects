a48e5578139f8b074ea57b73c3127bbe27659a8a
clintonbampoe <clintbamp@gmail.com>
2026-01-16 13:00:44 +0000

    Tuples: TCSA OOP Guide

--- Console Apps/LibraryManagementSystem/Program.cs
+++ Console Apps/LibraryManagementSystem/Program.cs
@@ -1,4 +1,7 @@
-﻿using LibraryManagementSystem;
+﻿//using LibraryManagementSystem;
 
-UserInterface userInterface = new();
-userInterface.MainMenu();
+//UserInterface userInterface = new();
+//userInterface.MainMenu();
+
+(string Title, int Pages) book1 = ("Frankenstein", 350);
+Console.WriteLine($"Title: {book1.Title}, Pages: {book1.Pages}");
