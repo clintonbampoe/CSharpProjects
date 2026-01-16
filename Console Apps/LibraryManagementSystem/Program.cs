063a6f811056e22691493f4fe87e962bcc1d31fe
clintonbampoe <clintbamp@gmail.com>
2026-01-16 00:09:48 +0000

    Objects: TCSA OOP Guide

--- Console Apps/LibraryManagementSystem/Program.cs
+++ Console Apps/LibraryManagementSystem/Program.cs
@@ -1,3 +1,23 @@
 ﻿using LibraryManagementSystem;
 
-UserInterface.MainMenu();
+// UserInterface.MainMenu();
+
+var car1 = new Car();
+var car2 = new Car();
+
+car1.name = "Ferrrari";
+car2.name = "BMW";
+
+car1.PrintName();
+car2.PrintName();
+
+class Car
+{
+    internal string name = "";
+
+    internal void PrintName()
+    {
+        name = name.ToUpper();
+        Console.WriteLine($"I'm a {name}");
+    }
+}
