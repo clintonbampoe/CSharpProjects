8e8a2ce0bad7be4c2df588d9e3ceb7d87178ca27
clintonbampoe <clintbamp@gmail.com>
2026-01-17 23:09:12 +0000

    Completed and successfully escapsulated database model into a single object

--- Console Apps/CodingTracking/Models/Database.cs
+++ Console Apps/CodingTracking/Models/Database.cs
@@ -11,22 +11,76 @@ class Database
         _connectionString = connectionString;
     }
 
-    public void Initialize()
+    internal void Initialize()
     {
-        using (SqliteConnection connection = new())
+        using (SqliteConnection connection = new(_connectionString))
         {
             connection.Open();
 
-            SqliteCommand createTable = connection.CreateCommand();
-            createTable.CommandText = 
+            string command = 
                 @"CREATE TABLE IF NOT EXISTS Sessions (
                     Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    StartTime TEXT NOT NULL,
-                    Duration INTEGER NOT NULL,
-                    Topic TEXT NOT NULL
+                    startTime TEXT NOT NULL,
+                    endTime TEXT NOT NULL,
+                    duration TEXT NOT NULL
                 );";
 
-            connection.Execute(createTable.CommandText);
+            connection.Execute(command);
         }
     }
+
+    internal void AddSession(CodingSession session)
+    {
+        using (SqliteConnection connection = new(_connectionString))
+        {
+            connection.Open();
+            string sql =
+            @"INSERT INTO Sessions (startTime, endTime, duration) VALUES (@StartTime, @EndTime, @Duration);";
+
+            connection.Execute(sql, session);
+        }
+    }
+
+    internal void EditSession(CodingSession session, int id)
+    {
+        using(SqliteConnection connection = new(_connectionString))
+        {
+            connection.Open();
+
+            string sql =
+            @"UPDATE Sessions 
+                SET startTime = @StartTime,
+                    endTime = @EndTime,
+                    duration = @Duration
+                WHERE id = @Id;";
+
+            connection.Execute(sql, session);
+        }
+    }
+
+    internal void DeleteSession(int id)
+    {
+        using(SqliteConnection connection = new(_connectionString))
+        {
+            connection.Open();
+
+            string sql =
+            @"DELETE Sessions
+                WHERE id = @Id;";
+
+            connection.Execute(sql, new {Id = id});
+        }
+    }
+    public IEnumerable<CodingSession> GetAllSessions()
+    {
+        using (SqliteConnection connection = new(_connectionString))
+        {
+            connection.Open();
+
+            return connection.Query<CodingSession>("SELECT * FROM Session");
+        }
+    }
+
+
+
 }
