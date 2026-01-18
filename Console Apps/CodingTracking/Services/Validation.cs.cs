5460f4a4cded9199e4e22e53e3a21533956f023a
clintonbampoe <clintbamp@gmail.com>
2026-01-18 12:51:38 +0000

    Added Validation class to business logic validation

---
+++ Console Apps/CodingTracking/Services/Validation.cs.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingTracker.Services;
+
+class Validation
+{
+    public bool IsValidSessionDuration (DateTime start, DateTime end) => _isValidSessionDuration(start, end);
+
+    private bool _isValidSessionDuration(DateTime startTime, DateTime endTime)
+    {
+        return (endTime > startTime);
+    }
+}
