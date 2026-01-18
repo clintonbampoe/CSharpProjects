a64000aa58abf8b543f2773a6b841c308dd9bf9c
clintonbampoe <clintbamp@gmail.com>
2026-01-18 11:18:59 +0000

    Updated CodingSession class, for easy maintainability

--- Console Apps/CodingTracking/Models/CodingSession.cs
+++ Console Apps/CodingTracking/Models/CodingSession.cs
@@ -1,14 +1,14 @@
 ﻿namespace CodingTracker.Models;
 class CodingSession
 {
-    public int SessionId { get; private set; }
+    public int Id { get; private set; }
     public DateTime StartTime { get; private set; }
     public DateTime EndTime { get; private set; }
     public TimeSpan Duration { get; private set; }
 
     public CodingSession(int sessionId, DateTime startTime, DateTime endTime)
     {
-        SessionId = sessionId;
+        Id = sessionId;
         StartTime = startTime;
         EndTime = endTime;
         Duration = EndTime - StartTime;
