a5c533d6a3a23d034b2296b4b32dbb882d451c23
clintonbampoe <clintbamp@gmail.com>
2026-01-18 11:18:09 +0000

    Added SessionController class

---
+++ Console Apps/CodingTracking/Controllers/SessionController.cs
@@ -0,0 +1,35 @@
+﻿using CodingTracker.Models;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+namespace CodingTracker.Controllers;
+
+class SessionController
+{
+    private Database _database;
+
+    public SessionController(Database database)
+    {
+        _database = database;
+    }
+
+    void AddSession()
+    {
+
+    }
+
+    void EditSession()
+    {
+
+    }
+
+    void DeleteSession()
+    {
+
+    }
+
+    void GetAllSessions()
+    {
+
+    }
+}
