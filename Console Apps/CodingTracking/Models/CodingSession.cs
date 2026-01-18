0465578e40b5210b55a5184d154e711524502af2
clintonbampoe <clintbamp@gmail.com>
2026-01-18 13:16:10 +0000

    updated session class to allow multiple constructor overloads

--- Console Apps/CodingTracking/Models/CodingSession.cs
+++ Console Apps/CodingTracking/Models/CodingSession.cs
@@ -6,9 +6,16 @@ class CodingSession
     public DateTime EndTime { get; private set; }
     public TimeSpan Duration { get; private set; }
 
-    public CodingSession(int sessionId, DateTime startTime, DateTime endTime)
+    public CodingSession( DateTime startTime, DateTime endTime)
     {
-        Id = sessionId;
+        StartTime = startTime;
+        EndTime = endTime;
+        Duration = EndTime - StartTime;
+    }
+
+    public CodingSession(int id, DateTime startTime, DateTime endTime)
+    {
+        Id = id;
         StartTime = startTime;
         EndTime = endTime;
         Duration = EndTime - StartTime;
