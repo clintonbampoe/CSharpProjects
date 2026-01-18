a99116dc2aa7038c2d07f9cccc74577478b235e7
clintonbampoe <clintbamp@gmail.com>
2026-01-18 12:58:58 +0000

    update validation class to make methods static for easy access to them

--- Console Apps/CodingTracking/Services/Validation.cs.cs
+++ Console Apps/CodingTracking/Services/Validation.cs.cs
@@ -2,9 +2,9 @@
 
 class Validation
 {
-    public bool IsValidSessionDuration (DateTime start, DateTime end) => _isValidSessionDuration(start, end);
+    public static bool IsValidSessionDuration (DateTime start, DateTime end) => _isValidSessionDuration(start, end);
 
-    private bool _isValidSessionDuration(DateTime startTime, DateTime endTime)
+    private static bool _isValidSessionDuration(DateTime startTime, DateTime endTime)
     {
         return (endTime > startTime);
     }
